5a01f696daf1c0173f9c1d424cd0354e5f700409
bszydlowski96 <bszydlowski96@gmail.com>
2025-09-02 01:44:40 +0200

    feat: Init React + TS + Storybook + Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,   // ⬅️ było 100
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "always"
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello React + TypeScript Prettier sprawdzenie testu</h1>
+      <p>
+        To jest dłuuugi tekst tylko po to, żeby Prettier złamał wiersz i ułożył kod na kilka linii
+        👌
+      </p>
+    </div>
+  )
+}
