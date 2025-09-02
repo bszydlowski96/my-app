8d4eaab914bab321ece4d72d9f847c76a8955c44
bszydlowski96 <bszydlowski96@gmail.com>
2025-09-02 02:27:30 +0200

    feat:[my-app] add scss modules + basic mixin (alert box)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import styles from "./App.module.scss"
+
 export default function App() {
   return (
     <div>
-      <h1>Hello React + TypeScript Prettier sprawdzenie testu</h1>
-      <p>
-        To jest dłuuugi tekst tylko po to, żeby Prettier złamał wiersz i ułożył kod na kilka linii
-        👌
-      </p>
+      <h1 className={styles.alert}>Hello SCSS Mixins 🚀</h1>
     </div>
   )
 }
